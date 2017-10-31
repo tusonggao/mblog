67a6493c1d6610a4ca79861b8639d5d60370d4d5
tusonggao <tusonggao@163.com>
2017-10-31 22:22:03 +0800

    make use of templates

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-
-
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
 
 # Register your models here.
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.template.loader import get_template
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
 
-# Create your views here.
+
+
+#def homepage(request):
+#    posts = Post.objects.all()
+#    post_lists = list()
+#    for count, post in enumerate(posts):
+#        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+#        post_lists.append("<small>" + str(post.body) 
+#                          + "</small><br><br>")
+#    return HttpResponse(post_lists)
+
+
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    
+    return HttpResponse(html)
+
+def showpost(request, slug):
+#    return HttpResponse('Hello world.')
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post!=None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
